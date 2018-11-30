0c474f89cae0b6c1f8661dcc85030847fea0617b
Ajani Burgos <32319193+AjaniBurgos@users.noreply.github.com>
2018-11-30 15:36:17 -0800

    Add files via upload
    Added menu, Seg faultin

--- cpe400finalproject/cpe400.cpp
+++ cpe400finalproject/cpe400.cpp
@@ -1,7 +1,11 @@
 // Program to find Dijkstra's shortest path using
 // priority_queue in STL
 #include<bits/stdc++.h>
+#include<iostream>
+#include<stdio.h>
+
 using namespace std;
+
 # define INF 0x3f3f3f3f
 
 // iPair ==>  Integer Pair
@@ -99,27 +103,26 @@ void Graph::shortestPath(int src)
 // Driver program to test methods of graph class
 int main()
 {
-    // create the graph given in above fugure
-    int V = 9;
-    Graph g(V);
+  //VARIABLES
+  int node_count=0;
+  int src, dest, dist = 0;
 
-    //  making above shown graph
-    g.addEdge(0, 1, 4);
-    g.addEdge(0, 7, 8);
-    g.addEdge(1, 2, 8);
-    g.addEdge(1, 7, 11);
-    g.addEdge(2, 3, 7);
-    g.addEdge(2, 8, 2);
-    g.addEdge(2, 5, 4);
-    g.addEdge(3, 4, 9);
-    g.addEdge(3, 5, 14);
-    g.addEdge(4, 5, 10);
-    g.addEdge(5, 6, 2);
-    g.addEdge(6, 7, 1);
-    g.addEdge(6, 8, 6);
-    g.addEdge(7, 8, 7);
+  Graph network(node_count);
 
-    g.shortestPath(0);
+  //INPUT
+  cout<<"Enter number of nodes for the network: ";
+  cin>>node_count;
+  //MENU
+  for(int index=0;index<node_count;index++)
+  {
+    cout<<"Enter Edge Information (Source, Destination, Distance): ";
+    scanf("%d,%d,%d", &src, &dest, &dist);
+    cout<<"POOP";
+    network.addEdge(src, dest, dist);
+    cout<<"ASS";
+  }
 
-    return 0;
-} 
+  network.shortestPath(0);
+
+  return 0;
+}
