6e991b6cb47aaa436b321f7822ea457f98e2bbaf
Joseph Stine <stinejoseph54@gmail.com>
2018-12-02 13:44:45 -0800

    Add files via upload
    added a test script that will print out the shortest path taken, vertex by vertex, then update the power with a decrement of -10

--- cpe400finalproject/cpe400.cpp
+++ cpe400finalproject/cpe400.cpp
@@ -16,6 +16,7 @@ class Graph
     Graph(int V);
     void addEdge(int u, int v, int w);
     void shortestPath(int s, vector<int>& power);
+    void test(int s,int dest, vector<int>& power);
 };
 
 Graph::Graph(int V)
@@ -63,6 +64,40 @@ void Graph::shortestPath(int src, vector<int>& power)
       printf("%d \t\t %d\n", i, dist[i]);
 }
 
+void Graph::test(int src,int dest, vector<int>& power)
+{
+  priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
+
+  vector<int> dist(V, INF);
+
+  pq.push(make_pair(0, src));
+  dist[src] = 0;
+
+  while (!pq.empty())
+  {
+    int u = pq.top().second;
+    pq.pop();
+
+    list< pair<int, int> >::iterator i;
+    for (i = adj[u].begin(); i != adj[u].end(); ++i)
+    {
+      int v = (*i).first;
+      int weight = (*i).second;
+
+      if (dist[v] > dist[u] + weight)
+      {
+        dist[v] = dist[u] + weight;
+        pq.push(make_pair(dist[v], v));
+        cout<< " " << v << ",";
+        power[v] = power[v] - 10;
+      }
+    }
+  }
+
+  printf("\nSource Node (%d)   Distance from node (%d)\n",src,dest);
+      printf("%d \t\t      %d\n", src, dist[dest]);
+}
+
 int main()
 {
   //VARIABLES
@@ -85,5 +120,15 @@ int main()
 
   network.shortestPath(0, power);
 
+
+  cout<<"Send a packet (Source Dest): ";
+  cin>>src>>dest;
+  cout<<"Route taken: ";
+  network.test(src,dest,power);
+
+  for(int i = 0 ; i < power.size() ; i++)
+  {
+    cout<<"Power remaining at node "<<i<< " :"<<power[i]<<endl;
+  }
   return 0;
 }
